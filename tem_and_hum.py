3847001f8fd24184a50f9a61b3e78fe4082fc1cf
dawncold <loooseleaves@gmail.com>
2014-08-23 07:38:51 +0800

    add max tried count, fixed gpio pin

--- tem_and_hum.py
+++ tem_and_hum.py
@@ -4,15 +4,20 @@
 import subprocess
 
 DHT_PROGRAM_PATH = './Adafruit_DHT'
-GPIO_PIN = '4'
+GPIO_PIN = '8'
 DHT_MODEL = '11'
+MAX_TRIED_COUNT = 3
 
 
 def read_from_dht():
     ret = None
+    tried_count = 1
     while 1:
         ret = subprocess.Popen(['sudo', DHT_PROGRAM_PATH, DHT_MODEL, GPIO_PIN]).communicate()[0]
         if ret:
+            break
+        tried_count += 1
+        if tried_count == MAX_TRIED_COUNT:
             break
 
 if __name__ == '__main__':
