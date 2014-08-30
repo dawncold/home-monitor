2a08ce54536805cd1879e6380c75a992ea78c69a
dawncold <loooseleaves@gmail.com>
2014-08-30 15:16:10 +0800

    upload local ftp

--- local_ftp.py
+++ local_ftp.py
@@ -6,10 +6,10 @@ FTP_ADDRESS = '192.168.2.1'
 FTP_USER = 'ftp'
 FTP_PASSWORD = 'ftp'
 FTP_UPLOAD_DIR = 'ftp'
-ftp = FTP(host=FTP_ADDRESS, user=FTP_USER, passwd=FTP_PASSWORD)
 
 
 def upload(name, f):
+    ftp = FTP(host=FTP_ADDRESS, user=FTP_USER, passwd=FTP_PASSWORD)
     ftp.cwd(FTP_UPLOAD_DIR)
     ftp.storbinary('STOR {}'.format(name), f)
     ftp.close()
